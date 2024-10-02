1a305617af48a92608b3319166f16ef534e743ef
tjk <tuomo.kondelin@gmail.com>
2024-10-02 16:55:37 +0300

    ensimmäinen pusku

--- public/script.js
+++ public/script.js
@@ -11,8 +11,23 @@ function setup(){
 function draw(){
     background("white")
     braw_paddle()
-    image(bomba,0,0,64,64)
 }
 function braw_paddle(){
     rect(mouseX,400,paddleWidth,paddleHeight);
+}
+
+class bombaa{
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.Width = 30;
+        this.Height = 30;
+        this.xSpeed = 1;
+        this.ySpeed = -1;
+        this.gravity = 0.05;
+    }
+    move(){
+        
+        image(bomba,this.x,this.y,this.Width,this.Height)
+    }
 }
