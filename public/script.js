32987e6ed20165fa4e7002c3ff40b04bca566515
tjk <tuomo.kondelin@gmail.com>
2024-10-23 17:01:00 +0300

    collision mailaan

--- public/script.js
+++ public/script.js
@@ -1,33 +1,53 @@
 let paddleWidth = 100
 let paddleHeight = 20
-let bomba;
+let paddleYposition = 400
+let bombaOlio;
+let screenHeight
+let screenWidth
+let bombaOlio2;
+let bombaimage;
 function preload(){
-    bomba = loadImage("image/bomba.png");
+    bombaimage = loadImage("image/bomba.png");
 }
 function setup(){
-    createCanvas(windowWidth,windowHeight);
+    screenHeight = windowWidth /3
+    screenWidth = windowWidth
+    createCanvas(screenWidth,screenHeight);
     background("black");
+    bombaOlio = new bombaa();
+    bombaOlio2 = new bombaa();
 }
 function draw(){
-    background("white")
+    background("red")
     braw_paddle()
+    bombaOlio.move();
+    bombaOlio2.move();
 }
 function braw_paddle(){
-    rect(mouseX,400,paddleWidth,paddleHeight);
+    rect(mouseX,paddleYposition,paddleWidth,paddleHeight);
 }
 
 class bombaa{
     constructor(){
         this.x = 0;
-        this.y = 0;
+        this.y = random(0,40);
         this.Width = 30;
         this.Height = 30;
-        this.xSpeed = 1;
+        this.xSpeed = random(5,10);
         this.ySpeed = -1;
         this.gravity = 0.05;
     }
     move(){
+        this.x = this.x + this.xSpeed;
+        this.ySpeed = this.ySpeed + this.gravity;
+        if(this.y > paddleYposition){
+            if(this.x > mouseX && this.x < mouseX + paddleWidth){
+
+                this.ySpeed = -abs(this.ySpeed) 
+            }           
+        }
+        this.y = this.y + this.ySpeed
+        image(bombaimage,this.x,this.y,this.Width,this.Height);
         
-        image(bomba,this.x,this.y,this.Width,this.Height)
     }
 }
