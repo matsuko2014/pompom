7573cc6eeff8579ed9f922331fb187b508d38add
tjk <tuomo.kondelin@gmail.com>
2024-11-13 17:00:51 +0200

    valmis

--- public/script.js
+++ public/script.js
@@ -1,43 +1,100 @@
+
+
 let paddleWidth = 100
 let paddleHeight = 20
-let paddleYposition = 400
-let bombaOlio;
+let paddleYposition = 50
 let screenHeight
 let screenWidth
-let bombaOlio2;
 let bombaimage;
+let kiviäimage
+let olioLista = [];
+//let bombaOlio;
+//let bombaOlio2;
+
+let score = 0
+let lives = 3
+let nappiWid
+
 function preload(){
     bombaimage = loadImage("image/bomba.png");
+    kiviäimage = loadImage("image/kiviä.JPG")
 }
 function setup(){
-    screenHeight = windowWidth /3
-    screenWidth = windowWidth
+    screenHeight = 450
+    screenWidth = 1250
+
+    paddleYposition = screenHeight - paddleHeight;
     createCanvas(screenWidth,screenHeight);
     background("black");
-    bombaOlio = new bombaa();
-    bombaOlio2 = new bombaa();
+    luo_olio();
+    //bombaOlio = new Ghost();
+    //bombaOlio2 = new Ghost();
 }
 function draw(){
-    background("red")
+    //backgraund("black")
+    bgimage()
     braw_paddle()
-    bombaOlio.move();
-    bombaOlio2.move();
+     //bombaOlio.Move();
+    //bombaOlio2.Move();
+    liikuta_olioita();
+    draw_pisteet()
 }
 function braw_paddle(){
     rect(mouseX,paddleYposition,paddleWidth,paddleHeight);
 }
+function bgimage(){
+    image(kiviäimage,0,0,screenWidth,screenHeight)
+}
+function luo_olio(){
+    let uusiOlio = new bombaa();
+    olioLista.unshift(uusiOlio);
+    //console.log(olioLista);
+    setTimeout(luo_olio,2000);
+}
+function liikuta_olioita(){
+    olioLista.forEach(function(olio,index){
+        olio.Move();
 
+        if(olio.y > screenHeight){
+            olioLista.splice(index,1);
+            lives = lives -1   
+            if(lives<= 0){
+                gameOver()
+            }
+        }
+
+        if(olio.x > screenWidth){
+            console.log("Lisätään piste");
+            olioLista.splice(index,1)
+            score = score + 1
+         }
+    })
+}
+function draw_pisteet(){
+textSize(30)
+fill("white")
+textAlign(LEFT, TOP)
+text('score:' + score + '\nlives:' + lives,5,10)
+}
+function gameOver(){
+    noLoop()
+    textSize(50)
+    textAlign(CENTER)
+    fill("white")
+    text("GAME OVER",screenWidth/2,screenHeight/2)
+}
 class bombaa{
     constructor(){
+        
         this.x = 0;
-        this.y = random(0,40);
+        this.y = random(0,50);
         this.Width = 30;
         this.Height = 30;
-        this.xSpeed = random(5,10);
+        this.xSpeed = random(3,5);
         this.ySpeed = -1;
         this.gravity = 0.05;
     }
-    move(){
+    Move(){
         this.x = this.x + this.xSpeed;
         this.ySpeed = this.ySpeed + this.gravity;
         if(this.y > paddleYposition){
@@ -48,6 +105,7 @@ class bombaa{
         }
         this.y = this.y + this.ySpeed
         image(bombaimage,this.x,this.y,this.Width,this.Height);
-        
+
+
+        }
     }
-}
